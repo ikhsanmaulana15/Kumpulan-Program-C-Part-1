2b5b547ced0759202b0ea91fe36d08843fbd422a
IKHSAN MAULANA <45529723+ikhsanmaulana15@users.noreply.github.com>
2018-12-03 01:10:21 +0700

    Add files via upload
    Contoh Sederhana Program C++ (File PDF)

---
+++ jarak.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int jarak, v, t;
+	cout <<"masukkan nilai kecepatan :";
+	cin >>v;
+	cout <<"masukkan nilai waktu :";
+	cin >>t;
+	jarak=v*t;
+	cout<<"nilai jarak adalah " <<jarak;
+	
+
+return 0;
+
+}
